3c5287a2d27b56b3ce817c3551211641665c11af
dust <du5t@multiplexed.be>
2016-02-07 22:53:34 -0800

    hooked up main-loop, virtual dom hyperscript, swarmlog

---
+++ index.js
@@ -0,0 +1,35 @@
+var h = require('virtual-dom/h')
+var vdom = require('virtual-dom')
+
+var main = require('main-loop')
+var loop = main({ msg: "nothing yet" }, render, vdom)
+document.querySelector('#content').appendChild(loop.target)
+
+
+function render (state) {
+  return h('div', [
+    h('h1', 'data from swarmlog: ' + state.msg || "no messages")
+ //   h('button', { onclick: onclick }, 'get data!')
+  ])
+//   function onclick () {
+//     loop.update({ times: state.times + 1 })
+//   }
+}
+
+var swarmlog = require('swarmlog')
+var memdb = require('memdb')
+
+var log = swarmlog({
+  publicKey: require('./keys.json').public,
+  sodium: require('chloride/browser'),
+  db: memdb(),
+  valueEncoding: 'json',
+  hubs: [ 'https://signalhub.mafintosh.com' ]
+})
+
+log.createReadStream({ live: true }).on('data', function (data) {
+  console.log('RECEIVED', data.value)
+  if (data.value.hasOwnProperty('msg')) {
+    loop.update({ msg: data.value.msg })
+  }
+})
---
+++ server.js
@@ -0,0 +1,17 @@
+var level = require('level-browserify')
+var swarmlog = require('swarmlog')
+
+var log = swarmlog({
+  keys: require('./keys.json'),
+  sodium: require('chloride/browser'),
+  db: level('server-db'),
+  valueEncoding: 'json',
+  hubs: [ 'https://signalhub.mafintosh.com' ]
+})
+
+var times = 0
+setInterval(function () {
+  console.log('sending data...')
+  log.append({ time: Date.now(), msg: 'HELLO!x' + times })
+  times++
+}, 1000)
