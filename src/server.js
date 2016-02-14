ca3834074beebe31613777a266bdce9276a38bad
dust <du5t@multiplexed.be>
2016-02-13 23:08:58 -0800

    got something hyperbooted

---
+++ src/server.js
@@ -0,0 +1,28 @@
+var level = require('level-browserify')
+var swarmlog = require('swarmlog')
+var sub = require('subleveldown')
+var db = require('memdb')() // replace with level for persistence
+
+var log = swarmlog({
+  keys: require('./keys.json'),
+  sodium: require('chloride/browser'),
+  db: sub(db, 'log'),
+  valueEncoding: 'json',
+//  hubs: [ 'https://signalhub.mafintosh.com' ],
+  hubs: [ 'http://localhost:8080' ]
+})
+
+var hyperkv = require('hyperkv')
+
+var kv = hyperkv({
+  log: log,
+  db: sub(db, 'kv')
+})
+
+var times = 0
+setInterval(function () {
+  console.log('sending data...')
+  var value = { time: Date.now(), msg: 'HELLO!x' + times }
+  kv.put('msg', value)
+  times++
+}, 1000)
